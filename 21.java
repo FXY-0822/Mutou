34af146bb6c510febec3778d03262b3e20063bef
FXY-0822 <1034108626@qq.com>
2019-11-27 19:51:34 +0800

    两数相加（链表 进位）

---
+++ 21.java
@@ -0,0 +1,15 @@
+package hello;
+ public class ListNode {
+     int val;
+     ListNode next;
+     ListNode(int x) { val = x; }
+ }
+
+class Solution {
+   public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+       ListNode pre=new ListNode();
+       
+   
+   
+   }
+}
